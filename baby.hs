8f5d9fbc659882fbfbe30cb962fba14240acefbf
Shane Richards <shane.richards121@gmail.com>
2015-08-22 12:46:17 -0500

    added a readme

--- baby.hs
+++ baby.hs
@@ -1,2 +1,19 @@
 doubleMe x = x + x
+
 doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else x * 2 
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+helloworld = "hello" ++ " " ++ "world"
+
+below10 = [1,2,3,4,5] ++ [6,7,8,9]
+
+woot = ['w', 'o'] ++ ['o', 't']
+
+below5 = 1:2:[3,4]
+
+aSmallCat = 'A':" SMALL CAT"
+
+
